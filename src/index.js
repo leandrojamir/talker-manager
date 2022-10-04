4aa4e96d9916ed18e18058fa6391244d8108e1ee
leandrojamir <leandrojamir@hotmail.com>
2022-10-04 18:38:51 -0300

    adcionando req1, teste local e thunderClient ok

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // iniciando projeto
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +13,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// 1 - Crie o endpoint GET /talker
+// requisição deve retornar o status 200 e um array com todas as pessoas palestrantes cadastradas.
+app.get('/talker', async (_req, res) => {
+  const pathTalker = path.resolve(__dirname, 'talker.json');
+  const talker = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
+  // Caso não exista nenhuma pessoa palestrante cadastrada a requisição deve retornar o status 200 e um array vazio.
+  const result = talker.length === 0 ? [] : talker;
+  res.status(200).json(result);
 });
 
 app.listen(PORT, () => {
