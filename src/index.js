8671f341aeb2a529c7251e500bf1485ca750d771
leandrojamir <leandrojamir@hotmail.com>
2022-10-11 11:42:13 -0300

    adcionando req4 teste local com docker ok teste no thnder client ok

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// iniciando projeto
+// terminar esse projeto em uma página proposital e voltar nele depois para aplicar MSC
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -49,6 +49,69 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(talkerID);
 });
 
+// 4 - Adicione as validações para o endpoint /login
+// Os campos recebidos pela requisição devem ser validados e, caso os valores sejam inválidos, o endpoint deve retornar o código de status 400 com a respectiva mensagem de erro ao invés do token.
+// As regras de validação são:
+// o campo email é obrigatório;
+// o campo email deve ter um email válido;
+// o campo password é obrigatório;
+// o campo password deve ter pelo menos 6 caracteres.
+// Os seguintes pontos serão avaliados:
+// Caso o campo email não seja passado ou esteja vazio, retorne um código de status 400 com o seguinte corpo:
+// {
+//   "message": "O campo \"email\" é obrigatório"
+// }
+// Caso o email passado não seja válido, retorne um código de status 400 com o seguinte corpo:
+// {
+//   "message": "O \"email\" deve ter o formato \"email@email.com\""
+// }
+// Caso o campo password não seja passado ou esteja vazio retorne um código de status 400 com o seguinte corpo:
+// {
+//   "message": "O campo \"password\" é obrigatório"
+// }
+// Caso a senha não tenha pelo menos 6 caracteres retorne um código de status 400 com o seguinte corpo:
+// {
+//   "message": "O \"password\" deve ter pelo menos 6 caracteres"
+// }
+const validarLogin1 = (req, res, next) => {
+  const { email } = req.body;
+  const regexSimples = /\S+@\S+\.\S+/;
+  if (!email) {
+    return res.status(400).json(
+      {
+        message: 'O campo "email" é obrigatório',
+      },
+    );
+  }
+  if (!regexSimples.test(email)) {
+    return res.status(400).json(
+      {
+        message: 'O "email" deve ter o formato "email@email.com"',
+      },
+    );
+  }
+  next();
+};
+// quebrando em 2 // eslint-disable-next-line max-lines-per-function
+const validarLogin2 = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json(
+      {
+        message: 'O campo "password" é obrigatório',
+      },
+    );
+  }
+  if (password.length <= 6) {
+    return res.status(400).json(
+      {
+        message: 'O "password" deve ter pelo menos 6 caracteres',
+      },
+    );
+  } 
+  next();
+};
+
 // 3 - Crie o endpoint POST /login
 // O endpoint deverá receber no corpo da requisição os campos email e password e retornar um token aleatório de 16 caracteres. Este token será utilizado pelas requisições dos próximos requisitos do projeto.
 // O corpo da requisição deverá ter seguinte formato:
@@ -65,7 +128,7 @@ app.get('/talker/:id', async (req, res) => {
 
 // https://levitrares.com/host-https-qastack.com.br/programming/8855687/secure-random-token-in-node-js
 // https://nodejs.org/api/crypto.html#cryptorandombytessize-callback
-app.post('/login', (req, res) => {
+app.post('/login', validarLogin1, validarLogin2, (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   const { email, password } = req.body;
 
