7a3b4f2cf59f78402799054978683e947eece1c3
leandrojamir <leandrojamir@hotmail.com>
2022-10-11 11:10:42 -0300

    retomando projeto adcionando req3 teste local com docker ok teste no thnder client ok

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -47,7 +48,32 @@ app.get('/talker/:id', async (req, res) => {
   }
   return res.status(200).json(talkerID);
 });
-  
+
+// 3 - Crie o endpoint POST /login
+// O endpoint deverá receber no corpo da requisição os campos email e password e retornar um token aleatório de 16 caracteres. Este token será utilizado pelas requisições dos próximos requisitos do projeto.
+// O corpo da requisição deverá ter seguinte formato:
+// {
+//   "email": "email@email.com",
+//   "password": "123456"
+// }
+// Os seguintes pontos serão avaliados:
+// O endpoint deverá retornar um código de status 200 com o token gerado e o seguinte corpo:
+// {
+//   "token": "7mqaVRXJSp886CGr"
+// }
+// O endpoint deve retornar um token aleatório a cada vez que for acessado.
+
+// https://levitrares.com/host-https-qastack.com.br/programming/8855687/secure-random-token-in-node-js
+// https://nodejs.org/api/crypto.html#cryptorandombytessize-callback
+app.post('/login', (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  const { email, password } = req.body;
+
+  if (email && password) {
+    res.status(200).json({ token });
+  }
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
